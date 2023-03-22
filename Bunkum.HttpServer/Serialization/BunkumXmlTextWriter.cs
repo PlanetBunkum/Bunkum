921cca26f3edd89d27c0a887e7264eab73926463
jvyden <jvyden@jvyden.xyz>
2023-03-21 20:05:15 -0400

    XML: Always skip attributes with namespaces

--- Bunkum.HttpServer/Serialization/BunkumXmlTextWriter.cs
+++ Bunkum.HttpServer/Serialization/BunkumXmlTextWriter.cs
@@ -20,4 +20,37 @@ public class BunkumXmlTextWriter : XmlTextWriter
 
     public override void WriteStartDocument() {}
     public override void WriteStartDocument(bool standalone) {}
+    
+    private bool _skipNextAttribute;
+    
+    public override void WriteStartAttribute(string? prefix, string localName, string? ns)
+    {
+        if (!string.IsNullOrEmpty(ns))
+        {
+            this._skipNextAttribute = true;
+            return;
+        }
+
+        base.WriteStartAttribute(prefix, localName, ns);
+        this._skipNextAttribute = false;
+    }
+
+    public override void WriteEndAttribute()
+    {
+        if (this._skipNextAttribute)
+        {
+            this._skipNextAttribute = false;
+            return;
+        }
+
+        base.WriteEndAttribute();
+    }
+
+    public override void WriteString(string? text)
+    {
+        if (!this._skipNextAttribute)
+        {
+            base.WriteString(text);
+        }
+    }
 }
