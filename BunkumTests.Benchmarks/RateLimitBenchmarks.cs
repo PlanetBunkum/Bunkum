9fbb6a21afbd884167e943cd46a808b245741b6a
jvyden <jvyden@jvyden.xyz>
2023-04-15 00:18:49 -0400

    Improve benchmark procedure

--- BunkumTests.Benchmarks/RateLimitBenchmarks.cs
+++ BunkumTests.Benchmarks/RateLimitBenchmarks.cs
@@ -3,41 +3,42 @@ using Bunkum.HttpServer;
 using Bunkum.HttpServer.RateLimit;
 using BunkumTests.HttpServer.Tests.RateLimit;
 using BunkumTests.HttpServer.Time;
+using JetBrains.Annotations;
 
 namespace BunkumTests.Benchmarks;
 
 [MemoryDiagnoser]
-[SimpleJob(invocationCount: 100)]
+[SimpleJob(invocationCount: 50_000)]
 public class RateLimitBenchmarks
 {
     private static readonly RequestContext Ctx = new();
     private RateLimiter _rateLimiter = null!;
     private MockTimeProvider _timeProvider = null!;
+    private MockRateLimitUser _user = null!;
 
-    [IterationSetup]
-    public void SetUp()
+    [GlobalSetup]
+    public void GlobalSetup()
     {
         this._timeProvider = new MockTimeProvider();
         this._rateLimiter = new RateLimiter(this._timeProvider);
     }
 
-    [Params(1, 2, 50, 100)]
-    public int Users { get; set; }
-    
-    [Params(1, 25, 50)]
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        this._user = new MockRateLimitUser("user");
+    }
+
+    [Params(1, 10, 25, 50, 100)]
+    [UsedImplicitly]
     public int Requests { get; set; }
 
     [Benchmark]
     public void RateLimit()
     {
-        // Technically shouldn't be allocating users here but i'm not sure how else to test
-        for (int i = 0; i < this.Users; i++)
+        for (int i = 0; i < this.Requests; i++)
         {
-            MockRateLimitUser user = new(i.ToString());
-            for (int j = 0; j < this.Requests; j++)
-            {
-                this._rateLimiter.ViolatesRateLimit(Ctx, user);
-            }
+            this._rateLimiter.ViolatesRateLimit(Ctx, this._user);
         }
     }
 }
