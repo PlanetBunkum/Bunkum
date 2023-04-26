99f658f916ff8e94e5b635e24a12442ea40eb2c2
jvyden <jvyden@jvyden.xyz>
2023-04-25 22:06:18 -0400

    Finish writing tests

--- BunkumTests.HttpServer/Tests/RateLimit/RateLimitIntegrationTests.cs
+++ BunkumTests.HttpServer/Tests/RateLimit/RateLimitIntegrationTests.cs
@@ -13,8 +13,6 @@ public class RateLimitIntegrationTests : ServerDependentTest
     public void AllowsSingleRequest()
     {
         MockTimeProvider timeProvider = new();
-        MockRateLimitUser user = new("user");
-
         RateLimiter rateLimiter = new(timeProvider);
         
         (BunkumHttpServer server, HttpClient client) = this.Setup();
@@ -27,5 +25,79 @@ public class RateLimitIntegrationTests : ServerDependentTest
             Assert.That(msg.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(await msg.Content.ReadAsStringAsync(), Is.EqualTo(TestEndpoints.TestString));
         });
+    }
+    
+    [Test]
+    public void AllowsManyRequests()
+    {
+        MockTimeProvider timeProvider = new();
+        RateLimiter rateLimiter = new(timeProvider);
+        
+        (BunkumHttpServer server, HttpClient client) = this.Setup();
+        server.AddEndpointGroup<TestEndpoints>();
+        server.AddService<RateLimitService>(rateLimiter);
+
+        for (int i = 0; i < RateLimiter.MaxRequestAmount; i++)
+        {
+            HttpResponseMessage msg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+            Assert.Multiple(async () =>
+            {
+                Assert.That(msg.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(await msg.Content.ReadAsStringAsync(), Is.EqualTo(TestEndpoints.TestString));
+            });
+        }
+    }
+    
+    [Test]
+    public void DeniesTooManyRequests()
+    {
+        MockTimeProvider timeProvider = new();
+        RateLimiter rateLimiter = new(timeProvider);
+        
+        (BunkumHttpServer server, HttpClient client) = this.Setup();
+        server.AddEndpointGroup<TestEndpoints>();
+        server.AddService<RateLimitService>(rateLimiter);
+
+        for (int i = 0; i < RateLimiter.MaxRequestAmount; i++)
+        {
+            HttpResponseMessage msg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+            Assert.Multiple(async () =>
+            {
+                Assert.That(msg.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(await msg.Content.ReadAsStringAsync(), Is.EqualTo(TestEndpoints.TestString));
+            });
+        }
+        
+        HttpResponseMessage badMsg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+        Assert.That(badMsg.StatusCode, Is.EqualTo(HttpStatusCode.TooManyRequests));
+    }
+    
+    [Test]
+    public void AllowsRequestAgainAfterTimeout()
+    {
+        MockTimeProvider timeProvider = new();
+        RateLimiter rateLimiter = new(timeProvider);
+        
+        (BunkumHttpServer server, HttpClient client) = this.Setup();
+        server.AddEndpointGroup<TestEndpoints>();
+        server.AddService<RateLimitService>(rateLimiter);
+
+        for (int i = 0; i < RateLimiter.MaxRequestAmount; i++)
+        {
+            HttpResponseMessage msg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+            Assert.Multiple(async () =>
+            {
+                Assert.That(msg.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(await msg.Content.ReadAsStringAsync(), Is.EqualTo(TestEndpoints.TestString));
+            });
+        }
+        
+        HttpResponseMessage newMsg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+        Assert.That(newMsg.StatusCode, Is.EqualTo(HttpStatusCode.TooManyRequests));
+
+        timeProvider.Seconds = 30;
+        
+        newMsg = client.Send(new HttpRequestMessage(HttpMethod.Get, "/"));
+        Assert.That(newMsg.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 }
--- BunkumTests.HttpServer/Tests/RateLimit/RateLimitTests.cs
+++ BunkumTests.HttpServer/Tests/RateLimit/RateLimitTests.cs
@@ -1,5 +1,4 @@
 using Bunkum.CustomHttpListener.Request;
-using Bunkum.HttpServer;
 using Bunkum.HttpServer.RateLimit;
 using BunkumTests.HttpServer.Time;
 
