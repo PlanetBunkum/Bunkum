b8cd8e51d752aa9c850d62943a065568b5ec8e4c
jvyden <jvyden@jvyden.xyz>
2023-09-26 19:13:11 -0400

    Document BunkumHttpListener

--- Bunkum.CustomHttpListener/BunkumHttpListener.cs
+++ Bunkum.CustomHttpListener/BunkumHttpListener.cs
@@ -12,20 +12,48 @@ namespace Bunkum.CustomHttpListener;
 /// </summary>
 public abstract class BunkumHttpListener : IDisposable
 {
+    /// <summary>
+    /// The logger.
+    /// </summary>
     protected readonly Logger Logger;
 
     private const int HeaderLineLimit = 1024; // 1KB per cookieHeader
+    
+    /// <summary>
+    /// The maximum length the request line's method can have in bytes.
+    /// </summary>
     protected const int RequestLineMethodLimit = 16; // bytes
+    
+    /// <summary>
+    /// The maximum length the request line's path can have in bytes.
+    /// </summary>
     protected const int RequestLinePathLimit = 128; // bytes
+    
+    /// <summary>
+    /// The maximum length the request line's version can have in bytes.
+    /// </summary>
     protected const int RequestLineVersionLimit = 16; // bytes
 
+    /// <summary>
+    /// Instantiates the listener, passing in the logger.
+    /// </summary>
+    /// <param name="logger">The logger to use for this listener.</param>
     protected BunkumHttpListener(Logger logger)
     {
         this.Logger = logger;
     }
 
+    /// <summary>
+    /// Tells the listener to start listening for requests. Called after startup tasks have completed.
+    /// </summary>
     public abstract void StartListening();
 
+    /// <summary>
+    /// Loop over <see cref="WaitForConnectionAsyncInternal"/> and wait for connections.
+    /// Handles errors cleanly, and calls back when a valid request comes through.
+    /// </summary>
+    /// <param name="action">The code to run when a valid request is made via this listener.</param>
+    /// <param name="globalCt">A cancellation token to allow for stopping listening.</param>
     public async Task WaitForConnectionAsync(Func<ListenerContext, Task> action, CancellationToken? globalCt = null)
     {
         while (globalCt is not { IsCancellationRequested: true })
@@ -55,6 +83,11 @@ public abstract class BunkumHttpListener : IDisposable
         }
     }
 
+    /// <summary>
+    /// Listen for an individual request and parses it.
+    /// </summary>
+    /// <param name="globalCt">A cancellation token to allow for stopping listening.</param>
+    /// <returns>A valid <see cref="ListenerContext"/> if the request is valid, null otherwise.</returns>
     protected abstract Task<ListenerContext?> WaitForConnectionAsyncInternal(CancellationToken? globalCt = null);
 
     internal static IEnumerable<(string, string)> ReadCookies(ReadOnlySpan<char> cookieHeader)
