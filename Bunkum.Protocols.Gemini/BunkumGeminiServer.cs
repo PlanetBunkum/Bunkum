4d3098b61ed1ad21517ffd1deafa3145150d8df8
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-10-21 13:57:50 -0700

    Gemini: Add logger configuration and logger sinks to BunkumGeminiServer constructor

--- Bunkum.Protocols.Gemini/BunkumGeminiServer.cs
+++ Bunkum.Protocols.Gemini/BunkumGeminiServer.cs
@@ -2,11 +2,15 @@ using Bunkum.Core;
 using Bunkum.Listener;
 using Bunkum.Protocols.Gemini.Socket;
 using NotEnoughLogs;
+using NotEnoughLogs.Sinks;
 
 namespace Bunkum.Protocols.Gemini;
 
 public class BunkumGeminiServer : BunkumServer
 {
+    public BunkumGeminiServer(LoggerConfiguration? configuration = null, List<ILoggerSink>? sinks = null) : base(configuration, sinks)
+    {}
+    
     protected override BunkumListener CreateDefaultListener(Uri listenEndpoint, bool useForwardedIp, Logger logger)
     {
         return new SocketGeminiListener(listenEndpoint, logger);
