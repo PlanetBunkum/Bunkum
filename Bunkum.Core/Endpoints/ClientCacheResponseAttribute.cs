435afcf4bfe065c950315178fb48d911e6abd2df
jvyden <jvyden@jvyden.xyz>
2024-05-23 16:34:57 -0400

    Document ClientCacheResponseAttribute, specify AttributeUsage

--- Bunkum.Core/Endpoints/ClientCacheResponseAttribute.cs
+++ Bunkum.Core/Endpoints/ClientCacheResponseAttribute.cs
@@ -6,20 +6,32 @@ namespace Bunkum.Core.Endpoints;
 /// Indicates that the client should cache the response for the given length.
 /// </summary>
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
+[AttributeUsage(AttributeTargets.Method)]
 public class ClientCacheResponseAttribute : Attribute
 {
     internal long Seconds { get; }
-
+    
+    /// <summary>
+    /// Indicates that the client should cache the response for 1 day.
+    /// </summary>
     public ClientCacheResponseAttribute()
     {
         this.Seconds = 86400; // 1 day
     }
-
+    
+    /// <summary>
+    /// Indicates that the client should cache the response for the given length.
+    /// </summary>
+    /// <param name="seconds">The number of seconds that the client should keep the response cached for</param>
     public ClientCacheResponseAttribute(long seconds)
     {
         this.Seconds = seconds;
     }
-
+    
+    /// <summary>
+    /// Indicates that the client should cache the response for the given length.
+    /// </summary>
+    /// <param name="span">The time span that the client should keep the response cached for</param>
     public ClientCacheResponseAttribute(TimeSpan span)
     {
         this.Seconds = span.Ticks / TimeSpan.TicksPerSecond;
