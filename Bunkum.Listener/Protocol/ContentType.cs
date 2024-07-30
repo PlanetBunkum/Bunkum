e7773108edf352d79114e19e84c1b98def426918
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-07-30 00:33:35 -0700

    Add more content types

--- Bunkum.Listener/Protocol/ContentType.cs
+++ Bunkum.Listener/Protocol/ContentType.cs
@@ -12,8 +12,21 @@ public static class ContentType
     public const string Html = "text/html; charset=utf-8";
     public const string Plaintext = "text/plain";
     public const string Xml = "text/xml";
+    
     public const string Json = "application/json";
     public const string BinaryData = "application/octet-stream";
+    
     public const string Png = "image/png";
+    public const string Apng = "image/apng";
     public const string Jpeg = "image/jpeg";
+    public const string Gif = "image/gif";
+    public const string Bmp = "image/bmp";
+    public const string Webp = "image/webp";
+
+    public const string Webm = "video/webm";
+    public const string Mp4 = "video/mp4";
+    public const string Mpeg = "video/mpeg";
+    public const string Flv = "video/x-flv";
+    
+    public const string Ogg = "audio/ogg";
 }
