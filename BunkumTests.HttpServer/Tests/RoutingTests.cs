1266db82ba60c7dea7d5e5a44de05b830e1fecaa
jvyden <jvyden@jvyden.xyz>
2025-06-24 21:35:30 -0400

    Fix tests

--- BunkumTests.HttpServer/Tests/RoutingTests.cs
+++ BunkumTests.HttpServer/Tests/RoutingTests.cs
@@ -14,7 +14,7 @@ public class RoutingTests : ServerDependentTest
         EndpointAttribute attribute = new HttpEndpointAttribute("/1234/1234");
         
         Assert.That(attribute.FullRoute, Is.EqualTo("/1234/1234"));
-        bool matches = attribute.UriMatchesRoute(new Uri("/1234/1234"), HttpProtocolMethods.Get, out Dictionary<string, string> parameters);
+        bool matches = attribute.UriMatchesRoute(new Uri("http://localhost/1234/1234"), HttpProtocolMethods.Get, out Dictionary<string, string> parameters);
         
         Assert.Multiple(() =>
         {
@@ -30,7 +30,7 @@ public class RoutingTests : ServerDependentTest
         
         Assert.That(attribute.FullRoute, Is.EqualTo("/1234/1234/_"));
         
-        bool matches = attribute.UriMatchesRoute(new Uri("/1234/1234/asdf"), HttpProtocolMethods.Get, out Dictionary<string, string> parameters);
+        bool matches = attribute.UriMatchesRoute(new Uri("http://localhost/1234/1234/asdf"), HttpProtocolMethods.Get, out Dictionary<string, string> parameters);
         Assert.Multiple(() =>
         {
             Assert.That(matches, Is.True);
